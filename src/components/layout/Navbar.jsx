ff9ebd84dcd64568f57f77c33ed4f1bc92562b02
Theed-DEV <louisjuste.theodore.jr@gmail.com>
2026-08-08 13:43:14 -0400

    Refactor Navbar component for cleaner JSX structure

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,25 +2,5 @@ import { Link, NavLink } from "react-router-dom";
 import "../../styles/navbar.css";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <div>
-        <Link to="/" className="logo" aria-label="Go to home">
-          theo<span>.dev</span>
-        </Link>
-      </div>
-      <div className="links">
-        <NavLink to="/">home</NavLink>
-        <NavLink to="/about">about</NavLink>
-        <NavLink to="/skills">skills</NavLink>
-        <NavLink to="/projects">projects</NavLink>
-        <NavLink to="/contact">contact</NavLink>
-      </div>
-      <div>
-        <Link to="/contact" className="btn">
-          // hire me
-        </Link>
-      </div>
-    </nav>
-  );
+  return <nav><div><Link to="/" className="logo" aria-label="Go to home">Theed<span>.dev</span></Link></div><div className="links"><NavLink to="/">home</NavLink><NavLink to="/about">about</NavLink><NavLink to="/skills">skills</NavLink><NavLink to="/projects">projects</NavLink><NavLink to="/contact">contact</NavLink></div><div><Link to="/contact" className="btn">// hire me</Link></div></nav>;
 }
