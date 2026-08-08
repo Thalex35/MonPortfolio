dfb627fab48836d2aba42acc61f8053db21ae760
Theed-DEV <louisjuste.theodore.jr@gmail.com>
2026-08-08 13:43:22 -0400

    Update footer text and brand name

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,5 @@
 import "../../styles/footer.css";
 
 export default function Footer() {
-  return (
-    <footer className="site_footer">
-      <p className="site_footer_brand">
-        theo<span>.dev</span>
-      </p>
-      <p className="site_footer_copy">Theodore 2026</p>
-    </footer>
-  );
+  return <footer className="site_footer"><p className="site_footer_brand">Theed<span>.dev</span></p><p className="site_footer_copy">Theed 2026</p></footer>;
 }
